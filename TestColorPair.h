1291bda756364de5dad013d97d6f888c33ebea93
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-01-17 21:33:13 +0530

    Update TestColorPair.h

--- TestColorPair.h
+++ TestColorPair.h
@@ -3,14 +3,17 @@
 #include "ColorPairHandler.h"
 #include "ColorInfos.h"
 
+namespace TelCoColorCoder
+{
 class TestColorPair
 {
 	public:
 
-	void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor);
+	void testNumberToPair(int pairNumber, ::TelCoColorCoder::MajorColor expectedMajor, ::TelCoColorCoder::MinorColor expectedMinor);
 
-	void testPairToNumber( TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor, int expectedPairNumber);
+	void testPairToNumber( ::TelCoColorCoder::MajorColor major, ::TelCoColorCoder::MinorColor minor, int expectedPairNumber);
 	
 	private:
 	ColorPairHandler m_colorPairHandler;
 };
+}
