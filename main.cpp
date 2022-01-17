7a7088dbd6f65f6281cea33ec50cd35914844993
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-01-17 23:53:41 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <assert.h>
-#include "TestColorPair.h"
+#include "ColorPairHandler.h"
 
 int main() {
-	TestColorPair testColorPair;
-    testColorPair.testNumberToPair(4, ::TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
-    testColorPair.testNumberToPair(5, ::TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
+	ColorPairHandler colorPairHandler;
+    colorPairHandler.testNumberToPair(4, ::TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
+    colorPairHandler.testNumberToPair(5, ::TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
-    testColorPair.testPairToNumber(::TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
-    testColorPair.testPairToNumber(::TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
+    colorPairHandler.testPairToNumber(::TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
+    colorPairHandler.testPairToNumber(::TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
 
     return 0;
 }
