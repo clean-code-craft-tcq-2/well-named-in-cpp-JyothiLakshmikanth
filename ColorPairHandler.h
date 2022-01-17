042600ffa81aee36733c9b9ad421845ce5e7e068
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-01-17 22:09:16 +0530

    Update ColorPairHandler.h

--- ColorPairHandler.h
+++ ColorPairHandler.h
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "ColorInfos.h"
 
+using namespace TelCoColorCoder;
+
 class ColorPairHandler
 {
 	public:
 	ColorPairHandler();
 	~ColorPairHandler();
-	::TelCoColorCoder::ColorPair GetColorFromPairNumber(int pairNumber);
-	int GetPairNumberFromColor(::TelCoColorCoder::MajorColor major, ::TelCoColorCoder::MinorColor minor);
+	ColorPair GetColorFromPairNumber(int pairNumber);
+	int GetPairNumberFromColor(MajorColor major, MinorColor minor);
 };
