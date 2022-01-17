4895a43d7aca041233c04c825c25ee970aa6ef95
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-01-17 23:41:45 +0530

    Update ColorPairHandler.h

--- ColorPairHandler.h
+++ ColorPairHandler.h
@@ -1,22 +1,10 @@
 #include <iostream>
 #include "ColorInfos.h"
 
-//using namespace TelCoColorCoder;
-
 class ColorPairHandler
 {
 	public:
-	ColorPairHandler();
-	~ColorPairHandler();
-	::TelCoColorCoder::ColorPair GetColorFromPairNumber(int pairNumber) {
-    int zeroBasedPairNumber = pairNumber - 1;
-   ::TelCoColorCoder::MajorColor majorColor = 
-        (::TelCoColorCoder::MajorColor)(zeroBasedPairNumber / (TelCoColorCoder::getNumberOfMinorColors()));
-    ::TelCoColorCoder::MinorColor minorColor =
-        (::TelCoColorCoder::MinorColor)(zeroBasedPairNumber % (TelCoColorCoder::getNumberOfMinorColors()));
-    return ::TelCoColorCoder::ColorPair(majorColor, minorColor);
-}
-	int GetPairNumberFromColor(::TelCoColorCoder::MajorColor major, ::TelCoColorCoder::MinorColor minor) {
-    return major * (TelCoColorCoder::getNumberOfMinorColors()) + minor + 1;
-}
+
+	::TelCoColorCoder::ColorPair GetColorFromPairNumber(int pairNumber);
+	int GetPairNumberFromColor(::TelCoColorCoder::MajorColor major, ::TelCoColorCoder::MinorColor minor) ;
 };
