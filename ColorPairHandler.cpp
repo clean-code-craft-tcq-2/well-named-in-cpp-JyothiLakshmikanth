67ac6e902fbccff4ab8ac529bfdd793e7bc1334f
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-01-17 23:29:23 +0530

    Update ColorPairHandler.cpp

--- ColorPairHandler.cpp
+++ ColorPairHandler.cpp
@@ -1,14 +1,14 @@
 #include "ColorPairHandler.h"
 
-::TelCoColorCoder::ColorPair ColorPairHandler::GetColorFromPairNumber(int pairNumber) {
-    int zeroBasedPairNumber = pairNumber - 1;
-   ::TelCoColorCoder::MajorColor majorColor = 
-        (::TelCoColorCoder::MajorColor)(zeroBasedPairNumber / (TelCoColorCoder::getNumberOfMinorColors()));
-    ::TelCoColorCoder::MinorColor minorColor =
-        (::TelCoColorCoder::MinorColor)(zeroBasedPairNumber % (TelCoColorCoder::getNumberOfMinorColors()));
-    return ::TelCoColorCoder::ColorPair(majorColor, minorColor);
-}
+// ::TelCoColorCoder::ColorPair ColorPairHandler::GetColorFromPairNumber(int pairNumber) {
+//     int zeroBasedPairNumber = pairNumber - 1;
+//    ::TelCoColorCoder::MajorColor majorColor = 
+//         (::TelCoColorCoder::MajorColor)(zeroBasedPairNumber / (TelCoColorCoder::getNumberOfMinorColors()));
+//     ::TelCoColorCoder::MinorColor minorColor =
+//         (::TelCoColorCoder::MinorColor)(zeroBasedPairNumber % (TelCoColorCoder::getNumberOfMinorColors()));
+//     return ::TelCoColorCoder::ColorPair(majorColor, minorColor);
+// }
 
-int ColorPairHandler::GetPairNumberFromColor(::TelCoColorCoder::MajorColor major, ::TelCoColorCoder::MinorColor minor) {
-    return major * (TelCoColorCoder::getNumberOfMinorColors()) + minor + 1;
-}
+// int ColorPairHandler::GetPairNumberFromColor(::TelCoColorCoder::MajorColor major, ::TelCoColorCoder::MinorColor minor) {
+//     return major * (TelCoColorCoder::getNumberOfMinorColors()) + minor + 1;
+// }
