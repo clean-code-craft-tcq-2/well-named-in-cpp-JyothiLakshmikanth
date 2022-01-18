a6eeca95386797f0dfff89a58e1e39b491bc30fa
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-01-18 19:02:29 +0530

    Update TestColorPair.h

--- TestColorPair.h
+++ TestColorPair.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <assert.h>
+#include <map>
 #include "ColorInfos.h"
 #include "ColorPairHandler.h"
 class TestColorPair
@@ -11,16 +12,15 @@ class TestColorPair
     TestColorPair(){}
    ~TestColorPair(){}
 
+    typedef ::std::map<int,TelCoColorCoder::ColorPair> T_ColorPairMap;
     void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor,TelCoColorCoder::MinorColor expectedMinor);
-
     void testPairToNumber( TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor, int expectedPairNumber);
+    void printTheComibination( TelCoColorCoder::MajorColor major);
 	
  };
 #endif
 
-void TestColorPair::testNumberToPair(int pairNumber,
-  TelCoColorCoder::MajorColor expectedMajor,
-   TelCoColorCoder::MinorColor expectedMinor)
+void TestColorPair::testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor)
 {
     ColorPairHandler colorPairHandler;
     TelCoColorCoder::ColorPair colorPair = colorPairHandler.GetColorFromPairNumber(pairNumber);
@@ -29,13 +29,22 @@ void TestColorPair::testNumberToPair(int pairNumber,
     assert(colorPair.getMinor() == expectedMinor);
 }
 
-void TestColorPair::testPairToNumber(
-    TelCoColorCoder::MajorColor major,
-    TelCoColorCoder::MinorColor minor,
-    int expectedPairNumber)
+void TestColorPair::testPairToNumber(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor, int expectedPairNumber)
 {
     ColorPairHandler colorPairHandler;
     int pairNumber = colorPairHandler.GetPairNumberFromColor(major, minor);
     std::cout << "Got pair number " << pairNumber << std::endl;
     assert(pairNumber == expectedPairNumber);
+}
+
+void TestColorPair::printTheComibination(TelCoColorCoder::MajorColor major)
+{
+    ColorPairHandler colorPairHandler;
+    T_ColorPairMap pair = colorPairHandler.GetTheComibination(major);
+    T_ColorPairMap::const_iterator pairIt = pair.begin();
+    for(;pairIt!= pair.end();++pairIt)
+    {
+        TelCoColorCoder::ColorPair colorPair = pairIt->second;
+        std::cout << pairIt->first <<" "<< colorPair.ToString().c_str() << std::endl;
+    }
 }
