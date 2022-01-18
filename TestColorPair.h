a28e568ba01407942cb8d99fe953b7c56dcda402
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-01-18 18:06:44 +0530

    Update TestColorPair.h

--- TestColorPair.h
+++ TestColorPair.h
@@ -1,13 +1,41 @@
-// #include "ColorPairHandler.h"
+#ifndef TESTCOLORPAIR_H_
+#define TESTCOLORPAIR_H_
 
-// class TestColorPair
-// {
-// 	public:
+#include <iostream>
+#include <assert.h>
+#include "ColorInfos.h"
+#include "ColorPairHandler.h"
+class TestColorPair
+ {
+    public:
+    TestColorPair(){}
+   ~TestColorPair(){}
 
-// 	void testNumberToPair(int pairNumber, ::TelCoColorCoder::MajorColor expectedMajor, ::TelCoColorCoder::MinorColor expectedMinor);
+    void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor,TelCoColorCoder::MinorColor expectedMinor);
 
-// 	void testPairToNumber( ::TelCoColorCoder::MajorColor major, ::TelCoColorCoder::MinorColor minor, int expectedPairNumber);
+    void testPairToNumber( TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor, int expectedPairNumber);
 	
-// 	private:
-// 	ColorPairHandler m_colorPairHandler;
-// };
+ };
+#endif
+
+void TestColorPair::testNumberToPair(int pairNumber,
+  TelCoColorCoder::MajorColor expectedMajor,
+   TelCoColorCoder::MinorColor expectedMinor)
+{
+    ColorPairHandler colorPairHandler;
+    TelCoColorCoder::ColorPair colorPair = colorPairHandler.GetColorFromPairNumber(pairNumber);
+    std::cout << "Got pair " << colorPair.ToString().c_str() << std::endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
+}
+
+void TestColorPair::testPairToNumber(
+    TelCoColorCoder::MajorColor major,
+    TelCoColorCoder::MinorColor minor,
+    int expectedPairNumber)
+{
+    ColorPairHandler colorPairHandler;
+    int pairNumber = colorPairHandler.GetPairNumberFromColor(major, minor);
+    std::cout << "Got pair number " << pairNumber << std::endl;
+    assert(pairNumber == expectedPairNumber);
+}
