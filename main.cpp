fea8ee2074348e06562cd0fae2970df93786d937
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-01-18 19:30:30 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,5 +12,10 @@ int main()
     test.testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
 
     test.printTheComibination(TelCoColorCoder::BLACK);
+    test.printTheComibination(TelCoColorCoder::WHITE);
+    test.printTheComibination(TelCoColorCoder::VIOLET);
+    test.printTheComibination(TelCoColorCoder::RED);
+    test.printTheComibination(TelCoColorCoder::YELLOW);
+
     return 0;
 }
