e3adac36d5b4d1760d50e3918594d2607afc6f13
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-01-24 21:12:24 +0530

    Update TestColorPair.h

--- TestColorPair.h
+++ TestColorPair.h
@@ -14,7 +14,6 @@ class TestColorPair
 
     void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor,TelCoColorCoder::MinorColor expectedMinor);
     void testPairToNumber( TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor, int expectedPairNumber);
-    void printTheComibination( TelCoColorCoder::MajorColor major);
 	
  };
 #endif
