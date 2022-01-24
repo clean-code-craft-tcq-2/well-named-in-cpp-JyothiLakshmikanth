b8f3ed20f2aa1927d9421a348c2aaa536f7526e8
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-01-24 21:01:12 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,20 +2,48 @@
 #include <assert.h>
 #include "TestColorPair.h"
 
+typedef ::std::map<int,TelCoColorCoder::ColorPair> T_ColorPairMap;
+T_ColorPairMap getTheCombination(TelCoColorCoder::MajorColor major)
+{
+    T_ColorPairMap colorPairMap;
+    for(int i = 0; i < TelCoColorCoder::numberOfMinorColors ; ++i)
+    {
+        TelCoColorCoder::MinorColor minorColor = (TelCoColorCoder::MinorColor)((i-1) % (TelCoColorCoder::numberOfMinorColors));
+        int pairNumber = GetPairNumberFromColor(majorColor, minorColor);
+        colorPairMap.insert({pairNumber, TelCoColorCoder::ColorPair(majorColor, minorColor)});) % (TelCoColorCoder::numberOfMinorColors));
+    }
+}
+
+T_ColorPairMap getColorPairMap()
+{
+    T_ColorPairMap colorMap;
+    for(int i=0;i<TelCoColorCoder::numberOfMajorColors ;++i)
+    {
+        colorMap = getTheCombination(MajorColorNames[i]);
+    }
+    return colorMap;
+}
+
+void printManual()
+{
+    T_ColorPairMap colorMap = getColorPairMap();
+    T_ColorPairMap::const_iterator colorMapIt = colorMap.begin();
+    for(; colorMapIt != colorMap.end(); ++colorMapIt)
+    {
+         ColorPair colorPair = colorMapIt->second
+         ::std::cout<<colorMapIt->first<<" "<<colorPair.ToString().c_str()<<std::endl;
+    }
+}
+
 int main()
 {
+    printManual();
     TestColorPair test;
     test.testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     test.testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
     test.testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     test.testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
-
-    test.printTheComibination(TelCoColorCoder::BLACK);
-    test.printTheComibination(TelCoColorCoder::WHITE);
-    test.printTheComibination(TelCoColorCoder::VIOLET);
-    test.printTheComibination(TelCoColorCoder::RED);
-    test.printTheComibination(TelCoColorCoder::YELLOW);
 
     return 0;
 }
